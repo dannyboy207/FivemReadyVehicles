869e197205e9e941c125c516f2060c9d5f345318
Deer <57545075+dannyboy207@users.noreply.github.com>
2023-01-14 13:00:18 +0100

    Add files via upload

---
+++ f22a/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+description 'F22A'
+
+files {
+    'data/**/*.meta'
+}
+ 
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/**/f22aweapons'
+data_file 'WEAPON_ARCHETYPES_FILE' 'data/**/weaponarchetypes'
